881ee606de15ee1e1d3f5d4a1368b951ecf66f4b
Dev Mehta <dpmehta02@gmail.com>
2013-06-04 11:49:50 -0700

    completed through learning curve

--- linreg/linearRegCostFunction.m
+++ linreg/linearRegCostFunction.m
@@ -1,16 +1,14 @@
 function [J, grad] = linearRegCostFunction(X, y, theta, lambda)
-%LINEARREGCOSTFUNCTION Compute cost and gradient for regularized linear 
-%regression with multiple variables
+% LINEARREGCOSTFUNCTION Compute cost and gradient for regularized linear 
+% regression with multiple variables
 %   [J, grad] = LINEARREGCOSTFUNCTION(X, y, theta, lambda) computes the 
 %   cost of using theta as the parameter for linear regression to fit the 
 %   data points in X and y. Returns the cost in J and the gradient in grad
 
-% Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
+J = 0; % Cost
+grad = zeros(size(theta)); % Gradient
 
 % linear hypothesis calc
 h_theta = X * theta;
@@ -18,7 +16,7 @@ h_theta = X * theta;
 % cost function
 J = 1 / (2 * m) * sum((h_theta - y) .^ 2);
 
-% the bias can't be regularizated... because it's a regularizor!
+% the bias can't be regularizated... because it's a regularizer!
 theta1 = [0 ; theta(2:size(theta), :)];
 
 % penalty (regularization)
--- linreg/linearRegression.m
+++ linreg/linearRegression.m
@@ -5,119 +5,97 @@ clear ; close all; clc
 
 %% =========== Part 1: Loading and Visualizing Data =============
 
-% Load Training Data
 fprintf('Loading and Visualizing Data ...\n')
 
 % Load train/test data
-% You need to define X, y, Xval, yval, Xtest, ytest
 % Fields: votes_useful  r_days_active   r_length  u_avg_votes_useful  sentiment
 % unused: order(created by me) r_exclamations  u_review_count
+% add misspellings/grammar issues as categories?
 data = csvread('train.csv');
-X = data(1:170000,2:5);
-y = data(1:170000,1);
+X = data(1:170,2:5); % UPDATE BACK TO X = data(1:170000,2:5);
+y = data(1:170,1); % UPDATE BACK TO y = data(1:170000,1); 
 
 testdata = csvread('test.csv');
 Xtest = testdata(:,2:5);
 ytest = testdata(:,1);
 
-% get test data in same format as train, then step through this file
-% add misspellings as category?
-
-Xval = data(170001:213435,2:5);
-Yval = data(170001:213435,1);
+Xval = data(213200:213435,2:5); % UPDATE BACK TO Xval = data(170001:213435,2:5);
+yval = data(213200:213435,1); % UPDATE BACK TO yval = data(170001:213435,1);
 
 % m = Number of examples
 m = size(X, 1);
+#{
+fprintf('Press enter to show plot 1.\n');
+pause;
 
 % Plot training data
-plot(X(:,1), y, 'rx', 'MarkerSize', 10, 'LineWidth', 1.5);
+plot(X(:,1), y, 'bo', 'MarkerSize', 10, 'LineWidth', 1.5);
 xlabel('Days active (review) (x)');
 ylabel('Votes useful (y)');
 
-fprintf('Program paused. Press enter to continue.\n');
+fprintf('Press enter to show plot 2.\n');
 pause;
 
-% Plot training data
-plot(X(:,2), y, 'rx', 'MarkerSize', 10, 'LineWidth', 1.5);
+plot(X(:,2), y, 'bo', 'MarkerSize', 10, 'LineWidth', 1.5);
 xlabel('Review length (x)');
 ylabel('Votes useful (y)');
 
-fprintf('Program paused. Press enter to continue.\n');
+fprintf('Press enter to show plot 3.\n');
 pause;
 
-% Plot training data
-plot(X(:,3), y, 'rx', 'MarkerSize', 10, 'LineWidth', 1.5);
+plot(X(:,3), y, 'bo', 'MarkerSize', 10, 'LineWidth', 1.5);
 xlabel('User avg votes useful (x)');
 ylabel('Votes useful (y)');
 
-fprintf('Program paused. Press enter to continue.\n');
+fprintf('Press enter to show plot 4.\n');
 pause;
 
-% Plot training data
-plot(X(:,4), y, 'rx', 'MarkerSize', 10, 'LineWidth', 1.5);
+plot(X(:,4), y, 'bo', 'MarkerSize', 10, 'LineWidth', 1.5);
 xlabel('AFINN sentiment score (x)');
 ylabel('Votes useful (y)');
 
-fprintf('Program paused. Press enter to continue.\n');
+fprintf('Program paused. Press enter to calculate cost function.\n');
 pause;
 
 %% =========== Part 2: Regularized Linear Regression Cost =============
 
-theta = [1 ; 1];
+theta = [1 ; 1 ; 1 ; 1 ; 1];
 J = linearRegCostFunction([ones(m, 1) X], y, theta, 1);
 
-fprintf(['Cost at theta = [1 ; 1]: %f '], J);
+fprintf(['Cost at theta (where lambda = 1) = [1 ; 1 ; 1 ; 1 ; 1]: %f \n'], J);
 
-fprintf('Program paused. Press enter to continue.\n');
+fprintf('Program paused. Press enter to calculate gradient.\n');
 pause;
 
 %% =========== Part 3: Regularized Linear Regression Gradient =============
-%  You should now implement the gradient for regularized linear 
-%  regression.
-%
 
-theta = [1 ; 1];
+theta = [1 ; 1 ; 1 ; 1 ; 1];
 [J, grad] = linearRegCostFunction([ones(m, 1) X], y, theta, 1);
 
-fprintf(['Gradient at theta = [1 ; 1]:  [%f; %f] '...
-         '\n(this value should be about [-15.303016; 598.250744])\n'], ...
-         grad(1), grad(2));
+fprintf(['Gradient at theta (where lambda = 1) = [1 ; 1 ; 1 ; 1 ; 1]:'...
+         '  [%f; %f; %f; %f; %f] \n'], ...
+         grad(1), grad(2), grad(3), grad(4), grad(5));
 
-fprintf('Program paused. Press enter to continue.\n');
+fprintf('Program paused. Press enter to train linear regression.\n');
 pause;
 
-
 %% =========== Part 4: Train Linear Regression =============
-%  Once you have implemented the cost and gradient correctly, the
-%  trainLinearReg function will use your cost function to train 
-%  regularized linear regression.
-% 
-%  Write Up Note: The data is non-linear, so this will not give a great 
-%                 fit.
-%
 
-%  Train linear regression with lambda = 0
 lambda = 0;
 [theta] = trainLinearReg([ones(m, 1) X], y, lambda);
 
 %  Plot fit over the data
-plot(X, y, 'rx', 'MarkerSize', 10, 'LineWidth', 1.5);
-xlabel('Change in water level (x)');
-ylabel('Water flowing out of the dam (y)');
+plot(X, y, 'bo', 'MarkerSize', 10, 'LineWidth', 1.5);
+xlabel('Inputs (x)');
+ylabel('Votes useful (y)');
 hold on;
 plot(X, [ones(m, 1) X]*theta, '--', 'LineWidth', 2)
 hold off;
 
-fprintf('Program paused. Press enter to continue.\n');
+fprintf('Program paused. Press enter to plot learning curves.\n');
 pause;
 
-
 %% =========== Part 5: Learning Curve for Linear Regression =============
-%  Next, you should implement the learningCurve function. 
-%
-%  Write Up Note: Since the model is underfitting the data, we expect to
-%                 see a graph with "high bias" -- slide 8 in ML-advice.pdf 
-%
 
 lambda = 0;
 [error_train, error_val] = ...
@@ -136,16 +114,14 @@ fprintf('# Training Examples\tTrain Error\tCross Validation Error\n');
 for i = 1:m
     fprintf('  \t%d\t\t%f\t%f\n', i, error_train(i), error_val(i));
 end
-
+#}
 fprintf('Program paused. Press enter to continue.\n');
 pause;
 
 %% =========== Part 6: Feature Mapping for Polynomial Regression =============
-%  One solution to this is to use polynomial regression. You should now
-%  complete polyFeatures to map each example into its powers
-%
 
-p = 8;
+% polynomial degree
+p = 2;
 
 % Map X onto Polynomial Features and Normalize
 X_poly = polyFeatures(X, p);
