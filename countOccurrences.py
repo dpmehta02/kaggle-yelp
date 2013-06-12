021565c9d6f8c6127a6a4e2ac15e1d095b3a8d98
Dev Mehta <dpmehta02@gmail.com>
2013-06-12 00:10:34 -0700

    skipping irrelevant newline breaks

--- countOccurrences.py
+++ countOccurrences.py
@@ -3,21 +3,26 @@
 # @dpmehta02
 # Count characters for each record in a csv file
 
-import sys
 import csv
-import os
+import re
 
 def main():
 
-  f = open('test.csv', 'r')
+  f = open('train_plus_test_reviews.csv', 'r')
   data = f.readlines()
   f.close()
 
   f = open('commmas.txt', 'w')
 
   for i in range(len(data)):
-    if data[i] != []:
-      f.write("%i\n" % data[i].count(","))
+    if data[i] != [] and data[i][0] == '\"':
+      count = 0
+      for char in data[i]:
+        if char == ",":
+          count += 1
+      f.write("%i\n" % count)
+
+  f.close()
 
 if __name__ == '__main__':
   main()
