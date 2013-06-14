05e92ef73c8f0686ba4bb3a883d4b4904e0421e7
Dev Mehta <dpmehta02@gmail.com>
2013-06-13 23:57:04 -0700

    deleted files

--- reviewProcessing.py
+++ reviewProcessing.py
@@ -2,35 +2,53 @@
 
 # @dpmehta02
 # Output commas, words, and word length for each yelp review
-# Usage: $ python reviewProcessing.py <output_file> <input_file>.json
+# Usage: $ python reviewProcessing.py <input_file>.json <output_file>
 
 import json
 import re
 import sys
 
-def main():
+def processReviews(json_file):
   
-  # output file
-  f = open(sys.argv[1], 'w')
-  # headers
-  f.write("comma_count,word_count,average_word_length\n")
-
-  # load the reviews
-  for line in open(sys.argv[2]):
+  # open output file
+  f = open(sys.argv[2], 'w')
+  
+  # write headers
+  f.write("comma_count,word_count,average_word_length,sentence_count,smilies\n")
+  
+  # load/process the reviews
+  for line in open(json_file):
+    
     review_json = json.loads(line)
     average_word_length = 0
+    
+    # if the review isn't blank
     if review_json['text'] != '':
-      comma_count = review_json['text'].encode('utf8').count(',')
-      word_count = len(review_json['text'].encode('utf8').split())
-
-      filtered = re.findall(r'\w+', review_json['text'].encode('utf8'))
+      
+      # factored out for clarity
+      utf_review = review_json['text'].encode('utf8')
+      
+      comma_count = utf_review.count(',')
+      word_count = len(utf_review.split())
+      # naive: assumes each ., ? and ! ends a sentence
+      sentence_count = utf_review.count('.') + utf_review.count('?') + utf_review.count('!')
+      
+      smilies = utf_review.count(':)') + utf_review.count('=)') + utf_review.count(':-)')
+      # strip punctuation
+      filtered = re.findall(r'\w+', utf_review)
+      # prevent divide-by-0 error
       if len(filtered) == 0:
         average_word_length == 0
       else:
         average_word_length = float(sum(len(word) for word in filtered))/len(filtered)
     
-    f.write("%i,%i,%f\n" % (comma_count, word_count, average_word_length))
+    f.write("%i,%i,%f,%i,%i\n" % (comma_count, word_count, average_word_length,sentence_count,smilies))
   f.close()
+
+
+def main():
+  # process the reviews
+  processReviews(sys.argv[1])
 
 if __name__ == '__main__':
   main()
