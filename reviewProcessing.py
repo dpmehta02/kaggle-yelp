b1f19289be804b91d3776741d8eed54f3bdd9215
Dev Mehta <dpmehta02@gmail.com>
2013-06-14 00:08:35 -0700

    added sentiment score

--- reviewProcessing.py
+++ reviewProcessing.py
@@ -7,20 +7,31 @@
 import json
 import re
 import sys
+import csv
 
 def processReviews(json_file):
   
   # open output file
   f = open(sys.argv[2], 'w')
   
+  # load a tab delimited dict of sentiment scores
+  afinnfile = open('AFINN-111.txt')
+  scores = {}
+  for line in afinnfile:
+    term, score  = line.split("\t")
+    scores[term] = int(score)
+
   # write headers
-  f.write("comma_count,word_count,average_word_length,sentence_count,smilies\n")
+  f.write("comma_count,word_count,average_word_length,sentence_count,smilies,sentiment\n")
   
   # load/process the reviews
   for line in open(json_file):
     
     review_json = json.loads(line)
+    
+    # default values
     average_word_length = 0
+    sentiment = 0
     
     # if the review isn't blank
     if review_json['text'] != '':
@@ -32,17 +43,24 @@ def processReviews(json_file):
       word_count = len(utf_review.split())
       # naive: assumes each ., ? and ! ends a sentence
       sentence_count = utf_review.count('.') + utf_review.count('?') + utf_review.count('!')
-      
+      # happy emoticons!
       smilies = utf_review.count(':)') + utf_review.count('=)') + utf_review.count(':-)')
       # strip punctuation
       filtered = re.findall(r'\w+', utf_review)
+      # look up/calculate sentiments
+      for word in filtered:
+        # only read alphanumeric words
+        if re.match("^[A-Za-z0-9_-]*$", word):
+          sentiment += scores.get(word, 0)
+
       # prevent divide-by-0 error
       if len(filtered) == 0:
         average_word_length == 0
       else:
         average_word_length = float(sum(len(word) for word in filtered))/len(filtered)
     
-    f.write("%i,%i,%f,%i,%i\n" % (comma_count, word_count, average_word_length,sentence_count,smilies))
+    f.write("%i,%i,%f,%i,%i,%i\n" % (comma_count, word_count, average_word_length,
+                                     sentence_count,smilies,sentiment))
   f.close()
 
 
