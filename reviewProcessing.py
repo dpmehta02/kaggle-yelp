aa1a24f9aff82d280e183e94a6c9c9100337455e
Dev Mehta <dpmehta02@gmail.com>
2013-06-24 17:36:54 -0700

    temporarilty removed user data calcuations

--- reviewProcessing.py
+++ reviewProcessing.py
@@ -6,14 +6,6 @@
 # Usage: $ python reviewProcessing.py <input_file>.json <output_file>
 ########################################################################
 
-# TODO - user avg votes useful (REPLACE MISSING WITH MEAN OR MEDIAN?)
-# visualize new data (to decide which features to transform)
-# TRAIN RANDOM FOREST REGRESSION sklearn
-# GENERATE SUBMISSION
-# THEN, UPLOAD TO OCTAVE AND RUN LINEAR REGRESSION, ONE-VS-ALL, ...
-# TRAIN ALGO WITH AND WITHOUT AVG VOTES USEFUL, APPLY TWO DIFFERENT ALGOS TO TEST SET
-# delete trailing newline char from file
-
 from datetime import datetime
 import json
 import re
@@ -37,6 +29,8 @@ def processReviews(json_file):
   # user_data[user_id] = (average_stars, review_count, avg_votes_useful)
   user_data = {}
 
+  # temporarily held out of calcuations
+  '''
   if sys.argv[1] == 'yelp_training_set_review.json':
     # load/process user data
     for line in open("./yelp_training_set_json/yelp_training_set_user.json"):
@@ -54,6 +48,7 @@ def processReviews(json_file):
       user_review_count = user_test_json['review_count']
       user_avg_votes_useful = 0
       user_data[user_test_json['user_id']] = (user_average_stars, user_review_count, user_avg_votes_useful)
+  '''
       
   # load/process the reviews
   for line in open(json_file):
@@ -106,6 +101,8 @@ def processReviews(json_file):
       else:
         average_word_length = float(sum(len(word) for word in filtered))/len(filtered)
 
+    # temporarily held out of calcuations
+    '''
     if user_id not in user_data:
       user_avg_stars = 0
       user_rev_count = 0
@@ -114,12 +111,12 @@ def processReviews(json_file):
       user_avg_stars = user_data[user_id][0]
       user_rev_count = user_data[user_id][1]
       user_average_votes_useful = user_data[user_id][2]
+    '''
 
     f.write("%s,%i,%i,%i,%i,%f,%i,%i,%i,%i,%.2f,%i,%.2f\n" % (user_id, votes_useful, days_active,
                                                  comma_count, word_count, average_word_length,
                                                  sentence_count, smilies, sentiment, 
-                                                 character_count, user_avg_stars, user_rev_count,
-                                                 user_average_votes_useful))
+                                                 character_count, 0,0,0))
   f.close()
 
 
